557166c95128b6c20ca60d88a2db6950d2680461
MANOSAI SEELAM <manosai2002@gmail.com>
2024-02-07 15:46:40 +0530

    Add files via upload

---
+++ groceryapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('login/',views.user_login,name='login'),
+    path('signup/',views.user_signup, name='signup'),
+    path('logout/',views.user_logout, name='logout'),
+    path('mycart/', views.mycart, name='add_to_cart'),
+    path('myorders/',views.placeOrder, name='add_to_order'),
+] 
